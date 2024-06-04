b41ed5a4176e561d6617aab4c11f95d1528665e5
Eslam El-Halawany <halawany.me@gmail.com>
2024-06-04 05:35:34 +0300

    Created app models

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    """This class representing Employee table"""
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    email = models.EmailField()
+    date_joined = models.DateField()
+    position = models.CharField(max_length=100)
+    salary = models.PositiveIntegerField() 
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
