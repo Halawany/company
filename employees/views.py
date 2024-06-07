b476548b87d815c33907bf0c65f4558f3f74b0e5
Eslam El-Halawany <halawany.me@gmail.com>
2024-06-07 03:17:33 +0300

    Update Employee Create view to accept list of dict(JSON)

--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework import generics 
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
 
 from .models import Employee
 from .serializers import EmployeeSerializer
@@ -11,6 +12,13 @@ class EmployeeView(generics.ListAPIView):
 class EmployeeCreateAPIView(generics.CreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class EmployeeUpdateView(generics.UpdateAPIView):
     queryset = Employee.objects.all()
